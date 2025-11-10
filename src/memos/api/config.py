d4c54ac136abe99e60c7a5066911ba2c147d0817
harvey_xiang <harvey_xiang22@163.com>
2025-11-10 20:09:15 +0800

    feat: update

--- src/memos/api/config.py
+++ src/memos/api/config.py
@@ -200,7 +200,8 @@ class NacosConfigManager:
         ak = os.getenv("AK")
         sk = os.getenv("SK")
 
-        if not (server_addr and data_id and ak and sk):
+        logger.info(f'NACOS_SERVER_ADDR: {server_addr}, NACOS_DATA_ID: {data_id}, NACOS_GROUP: {group}, NACOS_NAMESPACE: {namespace}, AK: {ak}, SK: {sk}')
+        if not (server_addr or data_id or ak or sk):
             logger.warning("❌ missing NACOS_SERVER_ADDR / AK / SK / DATA_ID")
             return
 
