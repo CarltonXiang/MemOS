9c2ceb23bfbbe020ff2e76f733f85c3facfa1a8f
harvey_xiang <harvey_xiang22@163.com>
2025-11-19 20:05:00 +0800

    feat: test

--- src/memos/log.py
+++ src/memos/log.py
@@ -187,7 +187,7 @@ LOGGING_CONFIG = {
     },
     "handlers": {
         "console": {
-            "level": selected_log_level,
+            "level": "DEBUG",
             "class": "logging.StreamHandler",
             "stream": stdout,
             "formatter": "no_datetime",
